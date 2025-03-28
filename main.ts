cc87f0255916b7df791a27538e19a283e9c1366c
Предраг Николић <idmpepe@gmail.com>
2025-03-28 19:13:57 +0100

    update ui

--- main.ts
+++ main.ts
@@ -11,15 +11,19 @@ await Deno.writeTextFile("index.html", `
     <meta charset="utf-8">
     <title>Find Color Scheme - Sublime Text</title>
 
-    <link rel="preload" href="main.css" as="style" />
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <link rel="preload" href="css/main.css" as="style" />
+    <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 
     <section class='first_section'>
-        <h1>Find Color Scheme</h1>
-        <p>for Sublime Text</p>
-        <input placeholder="bg color #FFFFFF" style="margin-top:4rem" maxlength="7" required>
+        <div class="noise-overlay"></div>
+        <div style="padding: 2rem">
+          <h1>Find Color Scheme</h1>
+
+          <p style="display: flex; align-items: center; justify-content: center; gap: 1rem">for <img src="img/st.png" height="50px" /></p>
+          <input placeholder="bg color #FFFFFF" style="margin-top: 2rem" maxlength="7" required>
+        </div>
         <section id='results' style="margin: 2rem 0"></section>
     </section>
 
